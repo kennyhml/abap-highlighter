76d16271d2b0a14b498cb725cbcf5834823d9a4a
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-11-25 22:21:01 +0100

    refactor(startup): Cleanup imports

--- src/de/kennyhml/e4/abap_syntax_highlighting/PluginStartup.java
+++ src/de/kennyhml/e4/abap_syntax_highlighting/PluginStartup.java
@@ -1,47 +1,21 @@
 package de.kennyhml.e4.abap_syntax_highlighting;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPartListener2;
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.texteditor.AbstractTextEditor;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.TextAttribute;
-import org.eclipse.jface.text.presentation.IPresentationReconciler;
+
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
-import org.eclipse.jface.text.rules.ICharacterScanner;
-import org.eclipse.jface.text.rules.IRule;
-import org.eclipse.jface.text.rules.IToken;
-import org.eclipse.jface.text.rules.IWordDetector;
-import org.eclipse.jface.text.rules.PatternRule;
-import org.eclipse.jface.text.rules.RuleBasedScanner;
-import org.eclipse.jface.text.rules.Token;
-import org.eclipse.jface.text.rules.WordRule;
 import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.jface.text.source.SourceViewer;
 import org.eclipse.jface.text.presentation.IPresentationRepairer;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Display;
 
-import com.sap.rnd.ui.syntaxcoloring.*;
-import com.sap.adt.programs.ui.internal.programs.editors.*;
 import com.sap.adt.tools.abapsource.ui.sources.editors.IAbapSourceMultiPageEditor;
 import com.sap.adt.tools.abapsource.ui.sources.editors.IAbapSourcePage;
-
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.jface.text.presentation.*;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class PluginStartup implements IStartup {
 
@@ -49,12 +23,10 @@ public class PluginStartup implements IStartup {
     private PresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer) {
         PresentationReconciler reconciler = new PresentationReconciler();
 
-        // Configure syntax highlighting for different content types
         DefaultDamagerRepairer dr = new DefaultDamagerRepairer(new AbapRuleBasedScanner());
         reconciler.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
         reconciler.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
 
-        // Add more damagers/repairers for other content types if needed
         return reconciler;
     }
 	
@@ -65,18 +37,13 @@ public class PluginStartup implements IStartup {
 		public void partActivated(org.eclipse.ui.IWorkbenchPartReference partRef) {
 	
 			 Display.getDefault().asyncExec(() -> {
-                 // You can replace this with any action, e.g., a pop-up dialog or a log entry
                  System.out.println("An editor has been opened: " + partRef.getId());
 			 });
 			 
 			if (partRef.getId().startsWith("com.sap.adt.abapClassEditor")) {
-                // Display a text message
-				
                 Display.getDefault().asyncExec(() -> {
-                    // You can replace this with any action, e.g., a pop-up dialog or a log entry
                     System.out.println("An abap class editor has been opened: " + partRef.getPartName());
                     
-                    // Get the editor instance
                     IWorkbenchPart part = partRef.getPart(false);
                     if (part instanceof IAbapSourceMultiPageEditor) {
                     	IAbapSourceMultiPageEditor editor = (IAbapSourceMultiPageEditor) part;
@@ -108,7 +75,5 @@ public class PluginStartup implements IStartup {
     	PlatformUI.getWorkbench().getDisplay().asyncExec(() -> {
     		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getPartService().addPartListener(listener);
         });
-        // Register your listener during startup
-        // ResourcesPlugin.getWorkspace().addResourceChangeListener(new ABAPAnnotationListener());
     }
 }
