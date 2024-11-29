e9f5b5b4541f701c07b14666b4963f6f1f14dd2f
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-11-29 22:42:56 +0100

    feat(types): Add type highlighting for ref to

--- src/de/kennyhml/e4/abap_syntax_highlighting/AbapIdentifierRule.java
+++ src/de/kennyhml/e4/abap_syntax_highlighting/AbapIdentifierRule.java
@@ -1,9 +1,12 @@
 package de.kennyhml.e4.abap_syntax_highlighting;
+
 import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.jface.text.rules.ICharacterScanner;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.IWordDetector;
 import org.eclipse.swt.graphics.Color;
+
+import de.kennyhml.e4.abap_syntax_highlighting.AbapToken.TokenType;
 
 public class AbapIdentifierRule extends AbapRegexWordRule {
 
@@ -25,7 +28,7 @@ public class AbapIdentifierRule extends AbapRegexWordRule {
 
 	public AbapIdentifierRule() {
 		super(new AbapIdentifierDetector());
-		
+
 		this.addWord("^[a-zA-Z_\\/][a-zA-Z0-9_\\/]*$", genericToken);
 	}
 
@@ -36,29 +39,25 @@ public class AbapIdentifierRule extends AbapRegexWordRule {
 		// Assign the last word we found to the token
 		if (!ret.isUndefined()) {
 			((AbapToken) ret).setAssigned(fLastWord);
-			
-			AbapToken prev = AbapScanner.getPreviousToken();
-			// Check if the identifier is a class type
-			if (prev != null && prev.getAbapType() == AbapToken.TokenType.KEYWORD) {
-				String prevTerm = prev.getLastAssignment();
-				if (prevTerm.equals("class")) {
-					ret = classToken;
-				} else if (prevTerm.equals("type") || prevTerm.equals("raising")) {
-					ret = typeToken;
-				}
+
+			// Check if previous token leads to a type, `ref to` are two tokens and need
+			// seperate checking. Only checking `to` would lead to mismatches.
+			if (AbapScanner.tokenMatchesAny(0, TokenType.KEYWORD, fTypeReferences)
+					|| (AbapScanner.tokenMatches(0, TokenType.KEYWORD, "to")
+							&& AbapScanner.tokenMatches(1, TokenType.KEYWORD, "ref"))) {
+				ret = typeToken;
 			}
-			AbapScanner.pushToken((AbapToken)ret);
+
+			AbapScanner.pushToken((AbapToken) ret);
 		}
 		return ret;
 	}
 
-
 	private static final Color GENERIC_COLOR = new Color(156, 220, 254);
-	private static final Color CLASS_COLOR = new Color(78, 201, 176);
 	private static final Color TYPE_COLOR = new Color(78, 201, 176);
-	private static final Color FIELD_COLOR = new Color(147, 115, 165);
-	
+
 	private AbapToken genericToken = new AbapToken(GENERIC_COLOR, AbapToken.TokenType.IDENTIFIER);
-	private AbapToken classToken = new AbapToken(CLASS_COLOR, AbapToken.TokenType.IDENTIFIER);
 	private AbapToken typeToken = new AbapToken(TYPE_COLOR, AbapToken.TokenType.IDENTIFIER);
+
+	private static String[] fTypeReferences = new String[] { "type", "raising", "class" };
 }
