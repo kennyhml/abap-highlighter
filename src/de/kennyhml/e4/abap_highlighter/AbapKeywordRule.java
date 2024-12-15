474b9b1419afb30713667893756cf3c3ea82155d
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-12-15 20:06:52 +0100

    fix(keywords): Add missing keywords

--- src/de/kennyhml/e4/abap_highlighter/AbapKeywordRule.java
+++ src/de/kennyhml/e4/abap_highlighter/AbapKeywordRule.java
@@ -21,9 +21,10 @@ public class AbapKeywordRule extends BaseAbapRule {
 
 		@Override
 		public boolean isWordPart(char c) {
-			return !Character.isWhitespace(c) && c != '\n' && c != '.' && c != '(' && c != ':';
+			return !fKeywordTerminators.contains((char)c);
 		}
-
+		
+		private static final Set<Character> fKeywordTerminators = Set.of(' ', '\n', '.', '(', ':');
 	}
 
 	@Override
@@ -37,33 +38,31 @@ public class AbapKeywordRule extends BaseAbapRule {
 		}
 
 		int c = scanner.read();
-		if (c == AbapScanner.EOF || !fDetector.isWordStart((char)c)) {
+		if (c == AbapScanner.EOF || !fDetector.isWordStart((char) c)) {
 			return Token.UNDEFINED;
 		}
-		
-		// Remember thatwe dont need to manually unread the word because the 
+
+		// Remember that we dont need to manually unread the word because the
 		// scanner will rollback the advanced characters if the token is undefined!
 		String text = scanner.readNext(c, fDetector);
 		if (!fKeywords.contains(text)) {
 			return Token.UNDEFINED;
 		}
-		
+
 		if (ctx.hasWord("types") && text.equals("of")) {
 			if (ctx.lastTokenMatches(TokenType.KEYWORD, "begin")) {
 				ctx.activate(ContextFlag.CONTEXT_STRUCT_DECL);
 			} else if (ctx.lastTokenMatches(TokenType.KEYWORD, "end")) {
 				ctx.deactivate(ContextFlag.CONTEXT_STRUCT_DECL);
 			}
-		} 
-		else if (fDataContextActivators.contains(text)) {
-			// This may be multi declaration too, that flag will be set by the delimiter rule.
+		} else if (fDataContextActivators.contains(text)) {
+			// This may be multi declaration too, that flag will be set by delimiter rule.
 			ctx.activate(ContextFlag.CONTEXT_DATA_DECL);
-		}
-		else if (fFuncContextActivators.contains(text)) {
+		} else if (fFuncContextActivators.contains(text)) {
 			// Could also be multi decl
 			ctx.activate(ContextFlag.CONTEXT_FUNC_DECL);
 		}
-		
+
 		fKeywordToken.setText(text);
 		ctx.addToken(fKeywordToken);
 		return fKeywordToken;
@@ -71,26 +70,28 @@ public class AbapKeywordRule extends BaseAbapRule {
 
 	private static final Set<String> fDataContextActivators = Set.of("data", "class-data", "parameters");
 	private static final Set<String> fFuncContextActivators = Set.of("methods", "class-methods");
-
+	
 	private static final Set<String> fKeywords = Set.of("if", "else", "elseif", "endif", "class", "endclass", "method",
 			"endmethod", "methods", "type", "types", "implementation", "definition", "data", "table", "of", "public",
 			"private", "protected", "section", "begin", "end", "final", "create", "is", "not", "initial", "and", "or",
 			"importing", "exporting", "changing", "returning value", "raising", "receiving", "line", "range", "loop",
-			"at", "endloop", "endwhile", "append", "appending", "fields", "to", "from", "select", "into",
-			"for", "all", "entries", "in", "where", "single", "value", "standard", "ref", "when", "write", "inheriting",
-			"returning", "class-methods", "class-data", "case", "others", "abstract", "assigning", "field-symbol",
-			"new", "try", "catch", "endtry", "join", "inner", "outer", "left", " right", "like", "update", "set",
-			"delete", "modify", "no-gaps", "condense", "concatenate", "on", "as", "raise", "exception", "constants",
-			"optional", "default", "call", "with", "non-unique", "unique", "key", "occurrences", "replace", "then",
-			"switch", "continue", "message", "corresponding", "sort", "by", "duplicates", "return", "function", "conv",
-			"exceptions", "reference", "preferred", "parameter", "length", "decimals", "empty", "components", "sorted",
-			"hashed", "seperated", "character", "mode", "respecting", "blanks", "byte", "include", "initialization",
+			"at", "endloop", "endwhile", "append", "appending", "fields", "to", "from", "select", "into", "for", "all",
+			"entries", "in", "where", "single", "value", "standard", "ref", "when", "write", "inheriting", "returning",
+			"class-methods", "class-data", "case", "others", "abstract", "assigning", "field-symbol", "new", "try",
+			"catch", "endtry", "join", "inner", "outer", "left", " right", "like", "update", "set", "delete", "modify",
+			"no-gaps", "condense", "concatenate", "on", "as", "raise", "exception", "constants", "optional", "default",
+			"call", "with", "non-unique", "unique", "key", "occurrences", "replace", "then", "switch", "continue",
+			"message", "corresponding", "sort", "by", "duplicates", "return", "function", "conv", "exceptions",
+			"reference", "preferred", "parameter", "length", "decimals", "empty", "components", "sorted", "hashed",
+			"seperated", "character", "mode", "respecting", "blanks", "byte", "include", "initialization",
 			"start-of-selection", "report", "selection-screen", "parameters", "lower", "obligatory", "select-options",
-			"block", "frame", "title", "intervals", "no", "starting", "visible", "checkbox", "user-command", "radiobutton",
-			"group", "listbox", "modif", "id", "screen");
+			"block", "frame", "title", "intervals", "no", "starting", "visible", "checkbox", "user-command",
+			"radiobutton", "group", "listbox", "modif", "id", "screen", "seperated", "split", "cond");
 
 	private static Color KEYWORD_COLOR = new Color(86, 156, 214);
+
 	private AbapToken fKeywordToken = new AbapToken(KEYWORD_COLOR, AbapToken.TokenType.KEYWORD);
-	
+
 	private AbapKeywordDetector fDetector = new AbapKeywordDetector();
-	}
+	
+}
